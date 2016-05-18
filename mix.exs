ab28d8480736ac10e93ae18771a35b206cf332f3
Aaron Jensen <aaronjensen@gmail.com>
2016-05-17 19:05:55 -0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exsyslog.Mixfile do
 
   def project do
     [app: :exsyslog,
-     version: "1.0.1",
+     version: "1.0.1+1",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
